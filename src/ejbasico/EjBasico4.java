f67682c6f55e66dbacc13a248b4a6656ec471529
Carmen Martín Núñez <cmartin@info2>
2022-09-27 10:46:19 +0200

    Initial commit

---
+++ src/ejbasico/EjBasico4.java
@@ -0,0 +1,17 @@
+package ejbasico;
+
+public class EjBasico4 {
+    public static void main(String[] args) {
+        //declaro la variable nota1 y le doy un valor
+        int nota1=5;
+
+        //declaro la variable nota2 y led oy un valor
+        int nota2=4;
+
+        //declaro la varable media donde calculo la media de las dos notas anteriores sumandolas y luego dividiendolas entre 2
+        double media=((double)nota1+(double)nota2)/2;
+
+        //Muestro la media por consola
+        System.out.println("La nota media del alumno es "+media);
+
+    }}
